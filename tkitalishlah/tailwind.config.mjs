da5767ca684ee45b8a18ed2fdf563f5169091480
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-01-31 10:05:19 +0000

    implement logo

--- tkitalishlah/tailwind.config.mjs
+++ tkitalishlah/tailwind.config.mjs
@@ -1,8 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Patrick Hand"', ...defaultTheme.fontFamily.sans],
+        heading: ['Chewy', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        brand: {
+          light: '#F9ECD7',
+          dark: '#48142C',
+        },
+      },
+    },
   },
   plugins: [],
 }
